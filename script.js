a09798b8924b3dc611965acd08414a60ebc4b06d
Jony Reyes <151489444+JonRe88@users.noreply.github.com>
2024-07-26 09:21:10 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.portfolio-item');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentSlide = 0;
+}
+
+
+}
